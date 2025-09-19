dcb7780e58a847b1e2ed37ed5b20532005864fce
yoyojunn <yoyongjun2005@gmail.com>
2025-09-19 22:14:19 +0800

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 import express from "express";
 import projectRoutes from "./projectRoutes.js";
+import cors from "cors";
 
 const app = express();
+
+//whitelist for CORS
+const allowedOrigins = [
+  "https://www.yoyojun.site", 
+  "http://localhost:5501"    
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    }
+  })
+);
+
 app.use(express.json());
 
 app.use('/api/v1', projectRoutes);
