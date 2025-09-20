a0170dd7a7f68938da07f135f5a11108b2a2a726
yoyojunn <yoyongjun2005@gmail.com>
2025-09-20 12:39:57 +0800

    feat: add POST and DELETE

--- src/projectRoutes.js
+++ src/projectRoutes.js
@@ -9,4 +9,24 @@ router.get('/projects', async (req, res) => {
   res.json(data);
 });
 
+router.post("/projects", async (req, res) => {
+  console.log(req.body);
+
+  const { slug, title, date, description, image, markdown_content, tool_icon1, tool_icon2 } = req.body;
+  const { data, error } = await supabase
+    .from("projects")
+    .insert([{ slug, title, date, description, image, markdown_content, tool_icon1, tool_icon2 }])
+    .select();
+  if (error) return res.status(500).json({ error: error.message });
+  res.status(201).json(data[0]);
+});
+
+router.delete("/projects/:id", async (req, res) => {
+  const { id } = req.params;
+  const { data, error } = await supabase.from("projects").delete().eq("id", id).select();
+  if (error) return res.status(500).json({ error: error.message });
+  if (data.length === 0) return res.status(404).json({ error: "Project not found" });
+  return res.status(200).json({ message: "Project deleted successfully" });
+});
+
 export default router;
